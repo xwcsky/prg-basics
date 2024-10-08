dffe0677880644cc7fcbb74943651af2ffaacdda
Wiktor Jurkowski <wicio100@interia.pl>
2024-10-08 11:57:05 +0200

    Sekcja 4 skonczona

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,6 @@
+name = "Wiktor"
+years = 20
+height = 184
+print(f"My name is {name}.")
+print(f"I am {years} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {years+6} years old.") 
